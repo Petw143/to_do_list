520f0bfb71496856ed55f2b7aedb689f2c8a8aad
Pedro Barbosa <barbosaph1805@gmail.com>
2025-04-16 16:56:37 -0300

    Criando a página principal do programa, a página de login

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,48 +10,107 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
+  bool _obscureText = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Pagina de Login'),
-      ),
       //backgroundColor: Theme.of(context).colorScheme.primary,
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          SizedBox(height: 40),
-          TextField(
-            controller: emailController,
-            decoration: InputDecoration(labelText: 'Email'),
-            keyboardType: TextInputType.emailAddress,
-          ),
-          SizedBox(height: 12),
-          TextField(
-            controller: passwordController,
-            decoration: InputDecoration(labelText: 'Senha '),
-            keyboardType: TextInputType.visiblePassword,
-            obscureText: true,
-          ),
-          SizedBox(height: 20),
-          Column(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              ElevatedButton(
-                child: Text('Entrar'),
-                onPressed: () => {},
+              const SizedBox(height: 10),
+              Text(
+                'To Do List',
+                style: TextStyle(fontFamily: 'AtheneVoyage', fontSize: 60),
               ),
-              const Spacer(),
-              Align(
-                child: Text(
-                  'Ainda nao tem uma conta?',
-                  style:
-                      TextStyle(color: Theme.of(context).colorScheme.primary),
-                ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(hintText: 'Email'),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  const SizedBox(height: 12),
+                  TextField(
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      hintText: 'Senha ',
+                      suffixIcon: IconButton(
+                        icon: Icon(_obscureText
+                            ? Icons.visibility_off
+                            : Icons.visibility),
+                        onPressed: () {
+                          setState(() {
+                            _obscureText = !_obscureText;
+                          });
+                        },
+                      ),
+                    ),
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: _obscureText,
+                  ),
+                  const SizedBox(height: 12),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: GestureDetector(
+                      onTap: () => {},
+                      child: Text(
+                        'Esqueceu sua senha?',
+                        style: TextStyle(color: Theme.of(context).primaryColor),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(1)),
+                        ),
+                        backgroundColor: Theme.of(context).colorScheme.primary,
+                      ),
+                      onPressed: () => {},
+                      child: Text(
+                        'Entrar',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 30),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Divider(),
+                  SizedBox(height: 5),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Você ainda não tem uma conta?'),
+                      GestureDetector(
+                        onTap: () => {},
+                        child: Text(
+                          ' Cadastre-se',
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.primary,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
               ),
             ],
-          )
-        ]),
+          ),
+        ),
       ),
     );
   }
